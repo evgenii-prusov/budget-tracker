72e446d6e6597ce91b9f2666bce2aceb69e2410d
Evgenii Prusov <prusove@gmail.com>
2025-12-24 19:10:59 +0100

    Implement Repository pattern for Account

---
+++ src/budget_tracker/db.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy import String, Numeric, Date
+from sqlalchemy.orm import registry, relationship
+
+from budget_tracker import model
+
+mapper_registry = registry()
+metadata = mapper_registry.metadata
+
+accounts = Table(
+    "account",
+    metadata,
+    Column("id", String, primary_key=True),
+    Column("name", String, nullable=False, unique=True),
+    Column("currency", String, nullable=False),
+    Column("initial_balance", Numeric, nullable=False),
+)
+
+transactions = Table(
+    "transaction",
+    metadata,
+    Column("id", String, primary_key=True),
+    Column("account_id", String, ForeignKey("account.id"), nullable=False),
+    Column("amount", Numeric, nullable=False),
+    Column("date", Date),
+    Column("category", String, nullable=False),
+    Column("category_type", String, nullable=False),
+)
+
+
+def start_mappers():
+    mapper_registry.map_imperatively(
+        model.Account,
+        accounts,
+        properties={
+            "_initial_balance": accounts.c.initial_balance,
+            "_transactions": relationship(
+                model.Transaction, backref="account"
+            ),
+        },
+    )
+    mapper_registry.map_imperatively(model.Transaction, transactions)
